0da88640a2153431c3625df5608712a3a40d5a24
Ana Valentim <31097110+AnaProgramando@users.noreply.github.com>
2023-04-08 18:25:43 -0300

    Create ContaCorrente.cs

---
+++ ContaCorrente.cs
@@ -0,0 +1,8 @@
+public class ContaCorrente
+{
+    public string titular;
+    public int agencia;
+    public int numConta;
+    //Valor padrão do saldo inicial da conta é de 100 e não 0
+    public double saldo = 100;
+}
